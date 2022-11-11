70e89da62095549ddfac67e7d2a5fa7e78b5da1a
Myungjae Lee <lmj7303@gmail.com>
2022-11-11 17:01:37 +0900

    django dstargram

--- 05_dstagram/photo/models.py
+++ 05_dstagram/photo/models.py
@@ -9,9 +9,23 @@ from django.db import models
 
 #장고의 기본 유저 모델
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+#외래키(ForeighKey)- User 테이블에서 해당 유저를 찾을 수 있는 주키
+#주키(PrimaryKey)-User 테이블에 1 admin X X X X
 class Photo(models.Model):
-    author=models.ForeignKey()
-    Photo
-    textcreated
-    updated
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_photos')
+    Photo= models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/no_image.png')
+    text= models.TextField()
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+
+#makemigrations->migrate
+class Meta:
+    ordering=['-updated']
+
+    def __str__(self):
+        return self.author.username+""+self.created.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_absolute_url(self):
+        return reverse('photo:photo_detail', args=[self.id])
--- 05_dstagram/photo/views.py
+++ 05_dstagram/photo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Photo
+
+def photo_list(request):
+    #보여줄 사진 데이터
+    photos=Photo.objects.all()
