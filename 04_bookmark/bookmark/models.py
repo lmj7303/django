ad742802bb4e6c41518deda8a96d96ff97d742f5
Myungjae Lee <lmj7303@gmail.com>
2022-11-11 11:36:54 +0900

    django

--- 04_bookmark/bookmark/models.py
+++ 04_bookmark/bookmark/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 #모델: 데이터베이스를 SQL없이 다루려고 모델을 사용
@@ -15,6 +16,11 @@ class Bookmark(models.Model):
     #2.제약사항(몇글자까지)
     #3.Form의 종류
     #4.Form에서 제약 사항
+    def __str__(self):
+        return "이름: "+self.site_name+", 주소: "+self.url
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
 #모델을 만들었다 => 데이터베이스에 어떤 데이터들을 어떤 형태로 넣을지 결정!
 #마이크레이션(migrate)=>데이터베이스에 모델의 내용을 반영(테이블 생성)
 #makemigrations=>모델의 변경사항을 추적해서 기록
