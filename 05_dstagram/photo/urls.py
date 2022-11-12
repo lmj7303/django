ab8dde26f79b6bbac5f2b8994e2077a5bb45a3ae
Myungjae Lee <lmj7303@gmail.com>
2022-11-12 22:23:25 +0900

    django dstargram

---
+++ 05_dstagram/photo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+#2차 URL 파일
+app_name='photo'
+
+urlpatterns=[
+    path('', photo_list, name='photo_list'),
+    path('upload/', PhotoUploadView.as_view(), name='photo_upload'),
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name='photo_delete'),
+]
