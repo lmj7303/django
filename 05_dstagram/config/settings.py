5ef4b749e46623337f7642b6a7e2450157f13fd8
Myungjae Lee <110644884+lmj7303@users.noreply.github.com>
2022-11-15 22:48:18 +0900

    django dstargram

--- 05_dstagram/config/settings.py
+++ 05_dstagram/config/settings.py
@@ -117,6 +117,9 @@ STATIC_URL = "static/"
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+# reverse_lazy 함수를 사용해서 다른 뷰를 적용할 수 있다.
+LOGIN_REDIRECT_URL='/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
